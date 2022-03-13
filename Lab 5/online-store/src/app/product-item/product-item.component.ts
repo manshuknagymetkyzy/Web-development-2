777297a62ec63e1287263153c2bbcfd3b3b653ba
Manshuk Nagymetkyzy <nagymetqyzy@gmail.com>
2022-03-13 11:00:14 +0600

    lab 5

---
+++ Lab 5/online-store/src/app/product-item/product-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent implements OnInit {
+  products = products;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
